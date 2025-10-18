6a8e9f10d1f643696fd4f8f17880df2a84795dc8
alison <alison.alves@academico.ifpb.edu.br>
2025-10-18 09:25:13 -0300

    Remover elemento de qualquer posição da lista

--- src/lista/ListaEncadeada.java
+++ src/lista/ListaEncadeada.java
@@ -7,6 +7,9 @@ public class ListaEncadeada<T> {
     private int tamanho;
 
     private final int NAO_ENCONTRADO = -1;
+    private final String NAO_EXISTE = "Posição não existe.";
+    private final String LISTA_VAZIA = "Lista vazia.";
+
 
     public void adiciona(T elemento) {
         No<T> novoNo = new No<T>(elemento);
@@ -33,8 +36,8 @@ public class ListaEncadeada<T> {
     }
 
     public void aciciona(int posicao, T elemento){
-        if (posicao < 0 || posicao > this.tamanho){
-            throw new IllegalArgumentException("Posição inválida.");
+        if (this.posicaoNaoExiste(posicao)){
+            throw new IllegalArgumentException(NAO_EXISTE);
         }
 
         if (posicao == 0){
@@ -52,7 +55,7 @@ public class ListaEncadeada<T> {
 
     public T removeInicio(){
         if (this.tamanho == 0){
-            throw new RuntimeException("Lista vazia.");
+            throw new RuntimeException(LISTA_VAZIA);
         }
 
         T removido = this.inicio.getElemento();
@@ -65,13 +68,41 @@ public class ListaEncadeada<T> {
         return removido;
     }
 
+    private boolean posicaoNaoExiste(int posicao){
+        return !(posicao >= 0 && posicao <= this.tamanho);
+    }
+
+    public T remove(int posicao){
+
+        if (this.posicaoNaoExiste(posicao)){
+            throw new IllegalArgumentException(NAO_EXISTE);
+        }
+
+        if (posicao == 0){
+            return this.removeInicio();
+        }
+
+        if(posicao == this.tamanho - 1){
+            return this.removeFinal();
+        }
+
+        No<T> noAnterior = this.buscaNo(posicao - 1);
+        No<T> atual = noAnterior.getProximo();
+        No<T> proximo = atual.getProximo();
+        noAnterior.setProximo(proximo);
+        atual.setProximo(null);
+        this.tamanho--;
+        return atual.getElemento();
+
+    }
+
     public int getTamanho() {
         return this.tamanho;
     }
 
     public T removeFinal(){
         if (this.tamanho == 0){
-            throw new RuntimeException("Lista vazia.");
+            throw new RuntimeException(LISTA_VAZIA);
         }
 
         if (this.tamanho == 1){
@@ -102,8 +133,8 @@ public class ListaEncadeada<T> {
 
     private No<T> buscaNo(int posicao){
 
-        if (!(posicao >= 0 && posicao < this.tamanho)){
-            throw new IllegalArgumentException("Posição não existe.");
+        if (this.posicaoNaoExiste(posicao)){
+            throw new IllegalArgumentException(NAO_EXISTE);
         }
 
         No<T> noAtual = this.inicio;
